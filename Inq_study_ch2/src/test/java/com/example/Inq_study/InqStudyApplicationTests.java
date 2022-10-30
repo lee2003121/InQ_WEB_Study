78741049e15cd05e45e07f50c67652c39ad1d531
이종완 <60810332+lee2003121@users.noreply.github.com>
2022-10-30 17:19:31 +0900

    2022_10_30_1719

---
+++ Inq_study_ch2/src/test/java/com/example/Inq_study/InqStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Inq_study;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InqStudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ Inq_study_ch3/src/main/java/com/example/Inq_study/InqStudyApplication.java
@@ -0,0 +1,13 @@
+package com.example.Inq_study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InqStudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InqStudyApplication.class, args);
+	}
+
+}
