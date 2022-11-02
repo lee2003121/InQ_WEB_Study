31e80c736da22c765fa04ca1a1db50571cb36beb
이종완 <60810332+lee2003121@users.noreply.github.com>
2022-11-03 02:11:28 +0900

    2022_11_03_0211

---
+++ Inq_study_ch4/src/main/java/com/example/Inq_study/service/MemberService.java
@@ -0,0 +1,43 @@
+package com.example.Inq_study.service;
+
+import com.example.Inq_study.domain.Member;
+import com.example.Inq_study.repository.MemberRepository;
+import com.example.Inq_study.repository.MemoryMemberRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public class MemberService {
+
+    private final MemberRepository memberRepository;
+
+    public MemberService(MemoryMemberRepository memberRepository)
+    {
+        this.memberRepository=memberRepository;
+    }
+    //회원가입
+    public Long join(Member member)
+    {
+        validateDuplicateMember(member);//중복 회원 검즘
+
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    private void validateDuplicateMember(Member member) {
+        memberRepository.findByName(member.getName())
+                .ifPresent(m->{
+                    throw new IllegalStateException("이미 존재하는 회원입니다.");
+        });
+    }
+
+    //전체 회원조회
+    public List<Member> findMembers() {
+       return memberRepository.findAll();
+    }
+
+    public Optional<Member> findOne(Long memberId)
+    {
+        return memberRepository.findById(memberId);
+    }
+}
