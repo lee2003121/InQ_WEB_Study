9da3500a854c83bfb1539c296e3e4aa0492c34a4
이종완 <60810332+lee2003121@users.noreply.github.com>
2022-11-13 15:57:19 +0900

    2022_11_13_1557

---
+++ Inq_study_ch7/src/main/java/com/example/Inq_study/SpringConfig.java
@@ -0,0 +1,23 @@
+package com.example.Inq_study;
+
+import com.example.Inq_study.repository.MemberRepository;
+import com.example.Inq_study.repository.MemoryMemberRepository;
+import com.example.Inq_study.service.MemberService;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SpringConfig {
+
+
+
+    @Bean
+    public MemberService memberService(){
+        return new MemberService(memberRepository());
+    }
+
+    @Bean
+    public MemberRepository memberRepository(){
+        return new MemoryMemberRepository();
+    }
+}
