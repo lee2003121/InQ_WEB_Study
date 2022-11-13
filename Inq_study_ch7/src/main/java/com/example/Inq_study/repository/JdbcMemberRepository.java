50039de054a49d4eecdcf3259e730649ff2f0752
이종완 <60810332+lee2003121@users.noreply.github.com>
2022-11-13 17:23:24 +0900

    2022_11_13_1723

--- Inq_study_ch7/src/main/java/com/example/Inq_study/repository/JdbcMemberRepository.java
+++ Inq_study_ch7/src/main/java/com/example/Inq_study/repository/JdbcMemberRepository.java
@@ -1,22 +1,17 @@
 package com.example.Inq_study.repository;
 
 import com.example.Inq_study.domain.Member;
-
 import org.springframework.jdbc.datasource.DataSourceUtils;
-
 import javax.sql.DataSource;
 import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
-
 public class JdbcMemberRepository implements MemberRepository {
     private final DataSource dataSource;
-
     public JdbcMemberRepository(DataSource dataSource) {
         this.dataSource = dataSource;
     }
-
     @Override
     public Member save(Member member) {
         String sql = "insert into member(name) values(?)";
@@ -42,7 +37,6 @@ public class JdbcMemberRepository implements MemberRepository {
             close(conn, pstmt, rs);
         }
     }
-
     @Override
     public Optional<Member> findById(Long id) {
         String sql = "select * from member where id = ?";
@@ -54,7 +48,7 @@ public class JdbcMemberRepository implements MemberRepository {
             pstmt = conn.prepareStatement(sql);
             pstmt.setLong(1, id);
             rs = pstmt.executeQuery();
-            if (rs.next()) {
+            if(rs.next()) {
                 Member member = new Member();
                 member.setId(rs.getLong("id"));
                 member.setName(rs.getString("name"));
@@ -68,7 +62,6 @@ public class JdbcMemberRepository implements MemberRepository {
             close(conn, pstmt, rs);
         }
     }
-
     @Override
     public List<Member> findAll() {
         String sql = "select * from member";
@@ -80,7 +73,7 @@ public class JdbcMemberRepository implements MemberRepository {
             pstmt = conn.prepareStatement(sql);
             rs = pstmt.executeQuery();
             List<Member> members = new ArrayList<>();
-            while (rs.next()) {
+            while(rs.next()) {
                 Member member = new Member();
                 member.setId(rs.getLong("id"));
                 member.setName(rs.getString("name"));
@@ -93,7 +86,6 @@ public class JdbcMemberRepository implements MemberRepository {
             close(conn, pstmt, rs);
         }
     }
-
     @Override
     public Optional<Member> findByName(String name) {
         String sql = "select * from member where name = ?";
@@ -105,7 +97,7 @@ public class JdbcMemberRepository implements MemberRepository {
             pstmt = conn.prepareStatement(sql);
             pstmt.setString(1, name);
             rs = pstmt.executeQuery();
-            if (rs.next()) {
+            if(rs.next()) {
                 Member member = new Member();
                 member.setId(rs.getLong("id"));
                 member.setName(rs.getString("name"));
@@ -118,12 +110,11 @@ public class JdbcMemberRepository implements MemberRepository {
             close(conn, pstmt, rs);
         }
     }
-
     private Connection getConnection() {
         return DataSourceUtils.getConnection(dataSource);
     }
-
-    private void close(Connection conn, PreparedStatement pstmt, ResultSet rs) {
+    private void close(Connection conn, PreparedStatement pstmt, ResultSet rs)
+    {
         try {
             if (rs != null) {
                 rs.close();
@@ -146,7 +137,6 @@ public class JdbcMemberRepository implements MemberRepository {
             e.printStackTrace();
         }
     }
-
     private void close(Connection conn) throws SQLException {
         DataSourceUtils.releaseConnection(conn, dataSource);
     }
